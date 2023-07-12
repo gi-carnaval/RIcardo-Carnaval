998def02124550738856e06224023c9ba49c218f
Giovani Carnaval <gi.carnaval91@gmail.com>
2023-07-11 21:23:33 -0300

    fix: atualiza caminho do globals.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../src/styles/globals.css'
 import { PrismicPreview } from '@prismicio/next'
 import { PrismicProvider } from '@prismicio/react'
 import type { AppProps } from 'next/app'
