58ea716f4e6e15e8a1f13f9a5c543d8e7ea02273
Giovani Carnaval <gi.carnaval91@gmail.com>
2023-07-11 23:16:52 -0300

    fix: atualiza chamada de cores adicionais ao projeto

--- components/molecules/NavMenuHeader.tsx
+++ components/molecules/NavMenuHeader.tsx
@@ -10,7 +10,7 @@ export function NavMenuHeader() {
 
   return (
     <nav className="w-full fixed top-0 left-0">
-      <div className="bg-bgDarkColor md:bg-transparent md:flex items-center justify-between py-4 md:py-6 h-24 md:px-20 px-7">
+      <div className="bg-darkColor md:bg-transparent md:flex items-center justify-between py-4 md:py-6 h-24 md:px-20 px-7">
         <Image
           alt="Ricardo Carnaval Logo Manuscrito"
           src={LogoRicardoCarnaval}
@@ -27,7 +27,7 @@ export function NavMenuHeader() {
           </button>
         </div>
         <ul
-          className={`md:flex md:items-center text-center md:gap-6 md:pb-0 pb-12 absolute md:static md:bg-transparent bg-bgDarkColor md:z-auto z-[-1] w-full left-0 md:w-auto md:pl-0 pl-9 transition-all duration-500 easy-in ${
+          className={`md:flex md:items-center text-center md:gap-6 md:pb-0 pb-12 absolute md:static md:bg-transparent bg-darkColor md:z-auto z-[-1] w-full left-0 md:w-auto md:pl-0 pl-9 transition-all duration-500 easy-in ${
             open ? 'top-16' : '-top-96'
           }`}
         >
--- components/organism/Header.tsx
+++ components/organism/Header.tsx
@@ -4,7 +4,7 @@ import { NavMenuHeader } from '../molecules/NavMenuHeader'
 
 export function Header() {
   return (
-    <header className="bg-[#323232]/70 backdrop-blur-md relative z-10">
+    <header className="bg-darkColor/70 backdrop-blur-md relative z-10">
       <div className="flex justify-between items-center px-6 md:px-24 md:py-6 h-24">
         <NavMenuHeader />
       </div>
