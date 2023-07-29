07295bb426b5026da89d6beef99b36581f376354
Giovani Carnaval <gi.carnaval91@gmail.com>
2023-07-29 20:36:02 -0300

    chore(core updates): atualização das configurações do projeto

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.prismic.io',
+        port: '',
+        pathname: '/ricardo-carnaval/**',
+      },
+    ],
+  },
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,14 @@ module.exports = {
         darkColor: '#202020',
         lightColor: '#DEDEDE',
       },
+      gridTemplateColumns: {
+        // Defina o número de colunas do grid aqui
+        gallery: 'repeat(auto-fit, minmax(200px, 1fr)',
+        '2-columns': 'repeat(2, minmax(0, 1fr))',
+      },
+      gridColumn: {
+        'span-2': 'span 2',
+      },
     },
     fontFamily: {
       sans: ['montserrat'],
