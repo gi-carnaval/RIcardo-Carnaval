89a22b8bc7848b0e027475c7681241d0e50cd239
Giovani Carnaval <gi.carnaval91@gmail.com>
2023-08-08 12:51:23 -0300

    refactor: altera diretorio /components

--- components/atoms/SectionH2Title.tsx
+++
@@ -1,9 +0,0 @@
-interface SectionTitleProps {
-  children: String
-}
-
-export function SectionH2Title(props: SectionTitleProps) {
-  return (
-    <h2 className="text-4xl text-center md:my-24 my-12">{props.children}</h2>
-  )
-}
